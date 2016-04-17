85d9aee39b78e05326eac482dc082f4c1cc429c0
Lokathor <zefria@gmail.com>
2016-04-17 14:48:32 -0600

    Flood fills to avoid disconnected caves.

--- src/Util/AutomataGen.hs
+++ src/Util/AutomataGen.hs
@@ -5,23 +5,24 @@ INCOMPLETE AT THE MOMENT
 -}
 module Util.AutomataGen where
 
-import Data.Vector (Vector)
-import qualified Data.Vector as V
-import Data.Vector.Mutable (STVector)
-import qualified Data.Vector.Mutable as VM
-
 import Control.Monad.ST
 import Control.Monad
 import Control.Monad.Random
 import Control.Monad.Random.Class
 import Control.Monad.Primitive
 import Control.Monad.Trans.Class
+import Control.RNG
 
+import Data.Vector (Vector)
+import qualified Data.Vector as V
+import Data.Vector.Mutable (STVector)
+import qualified Data.Vector.Mutable as VM
 import Data.Bool
 import Data.Location
 import Data.Tuple
-
-import Control.RNG
+import Data.Set (Set)
+import qualified Data.Set as S
+import Data.STRef
 
 {-| There's a lot of features a dungeon can have in a game, but for cellular
 automata work we just need to know what space we're working with and if there's
@@ -88,10 +89,14 @@ cellular automata computations with.
 mkRandom :: MonadRandom m => Int -> Int -> Int -> m (Vector Bool)
 mkRandom width height chance = V.replicateM (width*height) (rollChance chance)
 
+{-| @ mkCaves width height @
+
+Produces a SimpleDungeon based on a cellular automata technique.
+-}
 mkCaves :: MonadRandom m => Int -> Int -> m SimpleDungeon
 mkCaves width height = do
     base <- mkRandom width height 40
-    return $ SimpleDungeon width height $ runST $ do
+    let autoResult = runST $ do
         foo <- V.thaw base
         bar <- VM.replicate (width*height) False
         caveCopy width height foo bar
@@ -99,9 +104,45 @@ mkCaves width height = do
         caveCopy width height foo bar
         caveCopy width height bar foo
         caveCopy width height foo bar
-        --caveCopy width height bar foo
-        --caveCopy' width height foo bar
-        V.unsafeFreeze bar
+        maybeLoc <- findOpen bar
+        case maybeLoc of
+            Nothing -> return Nothing
+            Just i -> do
+                -- We set the destination to be all walls.
+                mapM_ (\i -> VM.unsafeWrite foo i True) [0 .. (width*height) -1]
+                -- Copy the open space into it
+                closedSet <- newSTRef (S.empty)
+                floodCopy8 width height bar foo closedSet i
+                Just <$> V.unsafeFreeze foo
+    case autoResult of
+        Nothing -> mkCaves width height
+        Just vec -> return $ SimpleDungeon width height vec
+
+findOpen :: PrimMonad m => VM.MVector (PrimState m) Bool -> m (Maybe Int)
+findOpen vec = do
+    let mid = VM.length vec `div` 2
+        indexes = [0 .. VM.length vec -1] :: [Int]
+        toCheck = drop mid indexes ++ take mid indexes -- this starts us in the middleish
+        go [] = return Nothing
+        go (x:xs) = do
+            isWall <- VM.unsafeRead vec x
+            if not isWall then return (Just x) else go xs
+    go toCheck
+
+floodCopy8 :: Int -> Int -> VM.MVector s Bool -> VM.MVector s Bool -> STRef s (Set Int) -> Int -> ST s ()
+floodCopy8 width height src dst closedSet i = do
+    already <- (S.member i) <$> readSTRef closedSet
+    wallHere <- VM.unsafeRead src i
+    -- true = wall, so we copy and spread out when we get a false
+    if not wallHere && not already
+        then do
+            modifySTRef' closedSet (S.insert i)
+            VM.unsafeWrite dst i False
+            let loc = fromIndex width i
+                indexes = inRange (width,height) loc 1
+            mapM_ (floodCopy8 width height src dst closedSet) indexes
+            --return ()
+        else return ()
 
 {-| @ inRange (width,height) (cx,cy) range @
 
@@ -116,22 +157,22 @@ inRange (width,height) (cx,cy) range = do
     guard $ not $ x < 0 || y < 0 || x >= width || y >= height
     return (x + y*width)
 
+{-| @ caveCopy width height src dst @
+
+Copies info from src to dst, not directly, but based on a cellular automata
+style system.
+-}
 caveCopy :: PrimMonad m => Int -> Int -> VM.MVector (PrimState m) Bool -> VM.MVector (PrimState m) Bool -> m ()
 caveCopy width height src dst = do
     forM_ [(x,y) | x <- [0 .. width-1], y <- [0 .. height-1]] $ \(x,y) -> do
         let oneIndexes = (inRange (width,height) (x,y) 1)
-        withinOne <- (\bools -> (9-length oneIndexes) + length (filter id bools)) <$> mapM (VM.read src) oneIndexes
-        let twoIndexes = (inRange (width,height) (x,y) 2)
-        withinTwo <- (\bools -> (25-length twoIndexes) + length (filter id bools)) <$> mapM (VM.read src) (inRange (width,height) (x,y) 2)
-        VM.write dst (x+y*width) (withinOne>=5 || withinTwo <=2)
-
-caveCopy' :: PrimMonad m => Int -> Int -> VM.MVector (PrimState m) Bool -> VM.MVector (PrimState m) Bool -> m ()
-caveCopy' width height src dst = do
-    forM_ [(x,y) | x <- [0 .. width-1], y <- [0 .. height-1]] $ \(x,y) -> do
-        let oneIndexes = (inRange (width,height) (x,y) 1)
-        withinOne <- (\bools -> (9-length oneIndexes) + length (filter id bools)) <$> mapM (VM.read src) oneIndexes
-        --withinTwo <- (length . filter id) <$> mapM (VM.read src) (inRange (width,height) (x,y) 2)
-        VM.write dst (x+y*width) (withinOne>=5)
+        withinOne <- (\bools -> (9-length oneIndexes) + length (filter id bools)) <$> mapM (VM.unsafeRead src) oneIndexes
+        if withinOne >= 5
+            then VM.unsafeWrite dst (x+y*width) True
+            else do
+                let twoIndexes = (inRange (width,height) (x,y) 2)
+                withinTwo <- (\bools -> (25-length twoIndexes) + length (filter id bools)) <$> mapM (VM.unsafeRead src) twoIndexes
+                VM.unsafeWrite dst (x+y*width) (withinTwo <=2)
 
 {-| @ fromIndex index width @
 
@@ -140,5 +181,9 @@ Turns an index into a location
 fromIndex :: Int -> Int -> (Int,Int)
 fromIndex width index = swap $ divMod index width
 
+{-| @ toIndex width (x,y) @
+
+Turns a location into an index
+-}
 toIndex :: Int -> (Int,Int) -> Int
 toIndex width (x,y) = (x + y*width)
